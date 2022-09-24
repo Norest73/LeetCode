efa9dcce3e22034ddf2956a44f513a6e5e15c751
Joon-hyung Park <59752447+Norest73@users.noreply.github.com>
2022-09-24 15:31:48 +0900

    Time: 21 ms (59.78%), Space: 7.7 MB (33.95%) - LeetHub

---
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.c
@@ -0,0 +1,15 @@
+int removeDuplicates(int* nums, int numsSize) {
+
+	if (numsSize == 1) return 1;
+	
+	size_t cnt = 0;
+
+	for (int i = 1; i < numsSize; i++) {
+		if (nums[i - 1] == nums[i])
+			cnt++;
+		else
+			nums[i - cnt] = nums[i];
+	}
+
+	return numsSize - cnt;
+}

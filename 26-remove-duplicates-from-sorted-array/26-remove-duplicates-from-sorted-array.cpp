834af06e364bab60469c3b74a27306fe8c418a95
Joon-hyung Park <59752447+Norest73@users.noreply.github.com>
2022-09-24 15:20:40 +0900

    Time: 13 ms (83.57%), Space: 18.5 MB (36.56%) - LeetHub

---
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using std::vector;
+using std::cout;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+
+        if (nums.size() == 1) { return 1; }
+        
+        size_t cnt = 0;
+        
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i-1] == nums[i])
+                cnt++;
+            else
+                nums[i - cnt] = nums[i];
+        }
+        
+        return nums.size() - cnt;
+    }
+};

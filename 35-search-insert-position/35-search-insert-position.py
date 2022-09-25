42693103296298f0c6f610093008807449400ff1
Joon-hyung Park <59752447+Norest73@users.noreply.github.com>
2022-09-25 17:12:26 +0900

    Time: 52 ms (93.54%), Space: 14.7 MB (40.65%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.py
@@ -0,0 +1,9 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        i = 0
+        while(nums[i] < target):
+            i += 1
+            if i == len(nums):
+                break
+        
+        return i

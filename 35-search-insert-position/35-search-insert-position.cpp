ac5c97608584eac0acff60fb822f35538ac9d714
Joon-hyung Park <59752447+Norest73@users.noreply.github.com>
2022-09-25 19:43:01 +0900

    Time: 14 ms (13.99%), Space: 9.8 MB (22.69%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+
+        size_t idx = 0;
+
+        while (nums[idx] < target) {
+            if (idx == nums.size()-1)
+                return nums.size();
+
+            idx++;
+        }
+        
+        return idx;
+    }
+};

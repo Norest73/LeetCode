8999385b85e80c840707697fc27a1d888af25396
Joon-hyung Park <59752447+Norest73@users.noreply.github.com>
2022-09-26 23:39:13 +0900

    Time: 5 ms (31.77%), Space: 6.5 MB (89.00%) - LeetHub

---
+++ 58-length-of-last-word/58-length-of-last-word.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+
+        while (s.back() == ' ') {
+            s.pop_back();
+        }
+
+        size_t cnt = 0;
+        while (!(s.empty()) && (s.back() != ' ')) {
+            s.pop_back();
+            cnt++;
+        }
+
+        return cnt;
+    }
+};

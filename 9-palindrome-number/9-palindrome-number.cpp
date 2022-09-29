27ef05eb7b352a2aca9da1964e425cb04d5e9c85
Joon-hyung Park <59752447+Norest73@users.noreply.github.com>
2022-09-29 22:38:30 +0900

    Time: 31 ms (33.07%), Space: 5.9 MB (74.09%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string temp = std::to_string(x);
+        int left = 0;
+        int right = temp.size() - 1;
+        
+        while (left <= right)
+        {
+            if (temp[left++] != temp[right--])
+                return false;
+        }     
+        return true;
+    }
+    
+};

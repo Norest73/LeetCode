904d2bb5317cbb44e63e13b4f652cb0abd248a87
Joon-hyung Park <59752447+Norest73@users.noreply.github.com>
2022-10-04 01:12:36 +0900

    Time: 9 ms (26.41%), Space: 5.8 MB (73.39%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+class Solution {
+public:
+    int mySqrt(int x) {
+
+        long long left = 0, right = x;
+        long long res = 0;
+        
+        while (left <= right) {
+            
+            long mid = (left + right) / 2;
+            
+            if (mid * mid == x)
+                return mid;
+
+            else if (mid * mid < x) {
+                res = mid;
+                left = mid + 1;
+            }
+            else
+                right = mid -1;
+        }
+        return res;
+    }
+};

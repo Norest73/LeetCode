c9533135bdc2a53a40dd47e725555812f581e31e
Joon-hyung Park <59752447+Norest73@users.noreply.github.com>
2022-10-04 01:13:36 +0900

    Time: 7 ms (59.06%), Space: 5.4 MB (85.68%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.c
@@ -0,0 +1,19 @@
+int mySqrt(int x){
+    long long left = 0, right = x;
+    long long res = 0;
+
+    while (left <= right) {
+
+        long long mid = (left + right) / 2;
+
+        if (mid * mid == x)
+            return mid;
+        else if (mid * mid < x) {
+            res = mid;
+            left = mid + 1;
+        }
+        else
+            right = mid -1;
+    }
+    return res;
+}

c7f1b8b961c4826530fbedf6f3d1ff104712c91a
Joon-hyung Park <59752447+Norest73@users.noreply.github.com>
2022-10-08 23:06:47 +0900

    Time: 3 ms (86.86%), Space: 6.1 MB (49.15%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.c
+++ 88-merge-sorted-array/88-merge-sorted-array.c
@@ -6,7 +6,6 @@ void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
             nums1[m + n - 1] = nums1[m - 1];
             m--;
         }
-        
         else {
             nums1[m + n - 1] = nums2[n - 1];
             n--;

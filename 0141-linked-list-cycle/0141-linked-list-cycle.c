021c0b5ccfd7a04df22873b82868995ee95dd6af
Joon-hyung Park <59752447+Norest73@users.noreply.github.com>
2022-10-16 23:28:25 +0900

    Time: 21 ms (37.44%), Space: 7.8 MB (58.61%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.c
+++ 0141-linked-list-cycle/0141-linked-list-cycle.c
@@ -13,7 +13,6 @@ bool hasCycle(struct ListNode *head) {
     while (fast != NULL && fast->next != NULL) {
         fast = fast->next->next;
         slow = slow->next;
-
         if (fast == slow)
             return true;
     }

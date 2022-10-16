b72c7b7e8d5231f8fdb74449ba73ba6520bf0554
Joon-hyung Park <59752447+Norest73@users.noreply.github.com>
2022-10-16 22:22:36 +0900

    Time: 107 ms (5.17%), Space: 20.8 MB (5.24%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <map>
+
+using std::vector;
+using std::map;
+using std::cout;
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        map<int, int> n_dict;
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            if (n_dict.find(nums[i]) != n_dict.end())
+                n_dict[nums[i]]++;
+            else
+                n_dict[nums[i]] = 1;
+        }
+
+        for (auto i = n_dict.begin(); i != n_dict.end(); i++) 
+            if (i->second == 1)
+                return i->first;
+        
+        return 0;
+    }
+};

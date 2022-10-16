0a6617b2a52d284d247d65f3663a646e1d554423
Joon-hyung Park <59752447+Norest73@users.noreply.github.com>
2022-10-16 23:28:52 +0900

    Time: 11 ms (89.97%), Space: 8.1 MB (18.03%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -17,7 +17,6 @@ public:
         while (fast != NULL && fast->next != NULL) {
             fast = fast->next->next;
             slow = slow->next;
-
             if (fast == slow)
                 return true;
         }

1f7f30d01593f6e20f4501bbca1afee5e94f1225
Joon-hyung Park <59752447+Norest73@users.noreply.github.com>
2022-10-30 23:46:21 +0900

    Time: 59 ms (12.29%), Space: 8.2 MB (7.40%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int cmp(const void* p, const void* q) {
+	
+	int a = *(int*)p, b = *(int*)q;
+	if (a > b)
+		return 1;
+	else
+		return -1;
+}
+
+int majorityElement(int* nums, int numsSize) {
+	qsort(nums, numsSize, sizeof(int), cmp);
+	return (nums[numsSize / 2]);
+}
